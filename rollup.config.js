0991a6ce2b7c405ca838a9cdc4ade41242b46695
dohaicuong <beatyshot@gmail.com>
2020-02-12 11:20:17 +1000

    clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import typescript from "rollup-plugin-typescript2";
-import sass from "rollup-plugin-sass";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+import typescript from 'rollup-plugin-typescript2'
+import sass from 'rollup-plugin-sass'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       sourcemap: true
     }
   ],
@@ -29,20 +29,20 @@ export default {
     }),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
+      exclude: '**/__tests__/**',
       clean: true
     }),
     commonjs({
-      include: ["node_modules/**"],
-      exclude: ["**/*.stories.js"],
+      include: ['node_modules/**'],
+      exclude: ['**/*.stories.js'],
       namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement"
+        'node_modules/react/react.js': [
+          'Children',
+          'Component',
+          'PropTypes',
+          'createElement'
         ],
-        "node_modules/react-dom/index.js": ["render"]
+        'node_modules/react-dom/index.js': ['render']
       }
     }),
     sass({
